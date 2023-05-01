27b92977605f34a12f92f8bfa0b3921c99e05e63
Andrey150 <ananevandrey150@gmail.com>
2023-05-01 22:30:33 +0300

    feat: add en array, add lang change

--- src/modules/keyboard.js
+++ src/modules/keyboard.js
@@ -5,7 +5,6 @@ export default function Keyboard() {
   keyBoard.classList.add('keyboard', 'ru');
   keyBoard.setAttribute('id', 'keyboard');
 
-
   const caps = keyBoard.classList.contains('caps');
 
   const lang = () => {
@@ -15,13 +14,27 @@ export default function Keyboard() {
     return symbolsEn
   }
 
-  Object.values(lang()).map(item => {
-    console.log(item.symbol.toUpperCase())
+  let isShiftPress = false;
+  let isAltPress = false;
+
+  function updateKeyboard(lang) {
+    const keys = document.querySelectorAll(".key");
+    keys.forEach((key, idx) => {
+      const item = lang()[idx];
+      key.innerHTML = item.symbol;
+    })
+  }
+
+  function renderKeyboard () {
+    Object.values(lang()).map(item => {
       keyBoard.insertAdjacentHTML('beforeend',
-  `<div class="key${item.specClass ? ' ' + item.specClass : ''}" data=${item.keyCode}>
+        `<div class="key${item.specClass ? ' ' + item.specClass : ''}" data=${item.keyCode}>
           ${caps && item.caps ? item.caps : item.symbol}
         </div>`);
-  });
+    });
+  }
+
+  renderKeyboard();
 
   document.addEventListener('keypress', function(event) {
     const key = document.querySelector(`.key[data="${event.keyCode}"]`);
@@ -35,11 +48,37 @@ export default function Keyboard() {
     }
   });
 
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'ShiftLeft') {
+      isShiftPress = true;
+    }
+    if (event.code === 'AltLeft') {
+      isAltPress = true;
+    }
+
+    if (isShiftPress && isAltPress) {
+      if (keyBoard.classList.contains('ru')) {
+        keyBoard.classList.replace('ru', 'en');
+        updateKeyboard(lang);
+      } else {
+        keyBoard.classList.replace('en', 'ru');
+        updateKeyboard(lang);
+      }
+    }
+  });
+
   document.addEventListener('keyup', function (event) {
     const key = document.querySelector(`.key[data="${event.keyCode}"]`);
 
     if (key) {
       key.classList.remove('active')
+    }
+
+    if (event.code === 'ShiftLeft') {
+      isShiftPress = false;
+    }
+    if (event.code === 'AltLeft') {
+      isAltPress = false;
     }
   })
 
